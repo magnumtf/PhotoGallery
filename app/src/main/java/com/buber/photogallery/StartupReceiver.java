605115b892a896baf3bed1dee471fca8a5dfcac8
magnumtf <magnumtf@yahoo.com>
2016-08-15 17:58:54 -0400

    initial version

---
+++ app/src/main/java/com/buber/photogallery/StartupReceiver.java
@@ -0,0 +1,21 @@
+package com.buber.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by t1004800 on 7/28/2016.
+ */
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
+    }
+}
